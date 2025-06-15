9e48cd73152c9fcd68645a420310719ea9c40ca7
cardsnship <cardsnship@gmail.com>
2025-06-15 00:58:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ const highlightedNews = shuffledNews.map(item =>
 );
 const loopedText = highlightedNews.join("  🔥🏈🔥  ") + "  🔥🏈🔥  " + highlightedNews.join("  🔥🏈🔥  ");
 marqueeInner.innerHTML = loopedText;
+console.log(loopedText);
 
 function setScrollSpeed() {
   const textWidth = marqueeInner.scrollWidth;
